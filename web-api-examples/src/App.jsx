50c7aed75cd0fd049b8ddac49bd72339053060dd
CarlsonYuan <carlsonyuandev@gmail.com>
2025-03-11 15:47:53 +0800

    Upgrade web sdk to 1.3.0 and using local cache

--- web-api-examples/src/App.jsx
+++ web-api-examples/src/App.jsx
@@ -2,52 +2,78 @@ import { useEffect, useState, useRef } from "react";
 import "./App.css";
 import AC from "agora-chat";
 
+import MiniCore from "agora-chat/miniCore/miniCore";
+import * as contactPlugin from "agora-chat/contact/contact";
+import * as localCachePlugin from "agora-chat/localCache/localCache";
+
+
+const APP_KEY = "61717166#1069763";
+const CONNECTION_MESSAGE_EVENT = "connection&message";
+const CHATROOM_EVENT = "ChatroomEvent";
+
+const miniCore = new MiniCore({
+  appKey: "61717166#1069763",
+});
+
 function App() {
-  // Replaces <Your app key> with your app key.
-  const appKey = "61717166#1069763";
   const [userId, setUserId] = useState("");
   const [token, setToken] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [peerId, setPeerId] = useState("");
   const [message, setMessage] = useState("");
   const [logs, setLogs] = useState([]);
-  const chatClient = useRef(null);
+  const [conversationId, setConversationId] = useState("");
+  const [conversationType, setConversationType] = useState("");
 
-  // Logs into Agora Chat.
   const handleLogin = () => {
     if (userId && token) {
-      chatClient.current.open({
-        user: userId,
-        accessToken: token,
+      miniCore.open({ username: userId, accessToken: token }).then(() => {
+        addLog("Login successful");
+      }).catch((error) => {
+        addLog(`Login failed: ${error.message}`);
       });
     } else {
       addLog("Please enter userId and token");
     }
   };
 
-  // Logs out.
   const handleLogout = () => {
-    chatClient.current.close();
-    setIsLoggedIn(false);
-    setUserId("");
-    setToken("");
-    setPeerId("");
+    miniCore.close().then(() => {
+      setIsLoggedIn(false);
+      setUserId("");
+      setToken("");
+      setPeerId("");
+      addLog("Logout successful");
+    }).catch((error) => {
+      addLog(`Logout failed: ${error.message}`);
+    });
   };
 
-   // Sends a peer-to-peer message.
-   const handleSendMessage = async () => {
+  const handleSendMessage = async () => {
     if (message.trim()) {
       try {
         const option = {
-          chatType: "singleChat", // Sets the chat type as a one-to-one chat.
-          type: "txt", // Sets the message type.
-          to: peerId, // Sets the recipient of the message with user ID.
-          msg: message, // Sets the message content.
+          chatType: conversationType || "singleChat",
+          type: "txt",
+          to: conversationId || peerId,
+          msg: message,
+          ext: {
+            em_apns_ext: {
+              em_push_mutable_content: true,
+              em_push_content_available: 1,
+              em_push_type: "voip",
+              em_push_sound: "default",
+            },
+            em_push_ext: {
+              type: "call",
+              custom: { key1: "value1", key2: "value2" },
+            }
+          },
         };
-        let msg = AC.message.create(option);
 
-        await chatClient.current.send(msg);
-        addLog(`Message send to ${peerId}: ${message}`);
+        const msg = AC.message.create(option);
+        await miniCore.send(msg);
+        addLog(`Message sent to ${conversationId || peerId}`);
         setMessage("");
       } catch (error) {
         addLog(`Message send failed: ${error.message}`);
@@ -57,191 +83,143 @@ function App() {
     }
   };
 
+  const getLocalConversations = async () => {
+    try {
+      const res = await miniCore.localCache.getLocalConversations();
+      console.log(res);
+    } catch (error) {
+      addLog(`Failed to get local conversations: ${error.message}`);
+    }
+  };
+
   const fetchConversationFromServer = async () => {
-    await chatClient.current.getServerConversations({pageSize:50, cursor: ''}).then((res)=>{
-      console.log(res)
-  })
-  }
-  
-  // Add log.
+    try {
+      const res = await miniCore.getServerConversations({ pageSize: 50, cursor: '' });
+      console.log(res);
+    } catch (error) {
+      addLog(`Failed to fetch conversations: ${error.message}`);
+    }
+  };
+
+  const getServerPinnedMessages = async () => {
+    const options = {
+      conversationId: '261322593861633',
+      conversationType: 'groupChat',
+      pageSize: 50,
+      cursor: '',
+    };
+    try {
+      const res = await miniCore.getServerPinnedMessages(options);
+      console.log(res);
+    } catch (error) {
+      addLog(`Failed to get pinned messages: ${error.message}`);
+    }
+  };
+
+  const checkIfUserInChatroomMuteList = async () => {
+    const res = await miniCore.isInChatRoomMutelist({
+      chatRoomId: '274471627849729',
+    })
+    console.log(res);
+  };
   const addLog = (log) => {
     setLogs((prevLogs) => [...prevLogs, log]);
   };
 
   useEffect(() => {
-    // Initializes the Web client.
-    chatClient.current = new AC.connection({
-      appKey: appKey,
-    });
 
-    // Adds the event handler.
-    chatClient.current.addEventHandler("connection&message", {
-      // Occurs when the app is connected to Agora Chat.
+    // Use the contact plugin, "contact" is a fixed value.
+    miniCore.usePlugin(contactPlugin, "contact");
+    // Use the local storage plugin, "localCache" is a fixed value.
+    miniCore.usePlugin(localCachePlugin, "localCache");
+
+    miniCore.addEventHandler(CONNECTION_MESSAGE_EVENT, {
       onConnected: () => {
         setIsLoggedIn(true);
-        addLog(`User ${userId} Connect success !`);
+        addLog(`User ${userId} connected successfully!`);
       },
-      // Occurs when the app is disconnected from Agora Chat.
       onDisconnected: () => {
         setIsLoggedIn(false);
-        addLog(`User Logout!`);
+        addLog("User logged out!");
       },
-      // Occurs when a text message is received.
       onTextMessage: (message) => {
         addLog(`${message.from}: ${message.msg}`);
-        let option = {
-          chatType: "singleChat", // The chat type: singleChat for one-to-one chat.
-          type: "channel", // The type of read receipt: channel indicates the conversation read receipt.
-          to: message.from, // The user ID of the message receipt.
+        const option = {
+          chatType: "singleChat",
+          type: "channel",
+          to: message.from,
         };
-        let msg = AC.message.create(option);
-        chatClient.current.send(msg);        
+        const msg = AC.message.create(option);
+        miniCore.send(msg);
       },
-      // Occurs when the token is about to expire.
-      onTokenWillExpire: () => {
-        addLog("Token is about to expire");
-      },
-      // Occurs when the token has expired.
-      onTokenExpired: () => {
-        addLog("Token has expired");
-      },
-      onError: (error) => {
-        addLog(`on error: ${error.message}`);
-      },
+      onTokenWillExpire: () => addLog("Token is about to expire"),
+      onTokenExpired: () => addLog("Token has expired"),
+      onError: (error) => addLog(`Error: ${error.message}`),
     });
 
-    chatClient.current.addEventHandler("ChatroomEvent", {
-      onChatroomEvent: function(msg){
-        switch(msg.operation){
-             // Occurs when all the chat room members are unmuted.
-            case 'unmuteAllMembers':
-                break;
-            // Occurs when all the chat room members are muted.
-            case 'muteAllMembers':
-                break;
-            // Occurs when a member is removed from the chat room allow list.
-            case 'removeAllowlistMember':
-                break;
-            // Occurs when a member is added to the chat room allow list.
-            case 'addUserToAllowlist':
-                break;
-            // Occurs when a member deletes a chat room announcement.
-            case 'deleteAnnouncement':
-                break;
-            // Occurs when a member updates a chat room announcement.
-            case 'updateAnnouncement':
-                break;
-            // Occurs when a member is removed from the chat room mute list.
-            case 'unmuteMember':
-                break;
-            // Occurs when a member is added to the chat room mute list.
-            case 'muteMember':
-                break;
-            // Occurs when a chat room admin is removed from the admin list.
-            case 'removeAdmin':
-                break;
-            // Occurs when a chat room member is added to the admin list.
-            case 'setAdmin':
-                break;
-            // Occurs when the chat room owner is changed.
-            case 'changeOwner':
-                break;
-            // Occurs when a chat room member leaves a chat room.
-            case 'memberAbsence':
-              addLog("memberAbsence");
-            // Occurs when a user joins a chat room.
-            case 'memberPresence':
-              addLog("memberPresence");
-            // Occurs when custom chat room attributes are set or changed.
-            case 'updateChatRoomAttributes':
-                break;
-            // Occurs when custom chat room attributes are removed.
-            case 'removeChatRoomAttributes':
-                break;
-            default:
-                break;
+    miniCore.addEventHandler(CHATROOM_EVENT, {
+      onChatroomEvent: (msg) => {
+        switch (msg.operation) {
+          case 'memberAbsence':
+            addLog("Member absence");
+            break;
+          case 'memberPresence':
+            addLog("Member presence");
+            break;
+          default:
+            break;
         }
-      }
-    })
-    
+      },
+    });
   }, []);
 
   return (
-    <>
-      <div
-        style={{
-          width: "500px",
-          display: "flex",
-          gap: "10px",
-          flexDirection: "column",
-        }}
-      >
-        <h2>Agora Chat Examples</h2>
-        {!isLoggedIn ? (
-          <>
-            <div>
-              <label>UserID: </label>
-              <input
-                type="text"
-                value={userId}
-                onChange={(e) => setUserId(e.target.value)}
-                placeholder="Enter the user ID"
-              />
-            </div>
-            <div>
-              <label>Token: </label>
-              <input
-                type="text"
-                value={token}
-                onChange={(e) => setToken(e.target.value)}
-                placeholder="Enter the token"
-              />
-            </div>
-            <button onClick={handleLogin}>Login</button>
-          </>
-        ) : (
-          <>
-            <h3>Welcome, {userId}</h3>
-            <button onClick={handleLogout}>Logout</button>
-            <div>
-              <label>Peer userID: </label>
-              <input
-                type="text"
-                value={peerId}
-                onChange={(e) => setPeerId(e.target.value)}
-                placeholder="Enter the peer user ID"
-              />
-            </div>
-            <div>
-              <label>Peer message: </label>
-              <input
-                type="text"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                placeholder="Input message"
-              />
-              <button onClick={handleSendMessage}>Send</button>
-            </div>
-          </>
-        )}
-      <button onClick={fetchConversationFromServer}>fetchConversationFromServer</button>
-        <h3>Operation log</h3>
-        <div
-          style={{
-            height: "300px",
-            overflowY: "auto",
-            border: "1px solid #ccc",
-            padding: "10px",
-            textAlign: "left",
-          }}
-        >
-          {logs.map((log, index) => (
-            <div key={index}>{log}</div>
-          ))}
-        </div>
-      </div>
-    </>
+    <div style={{ width: "500px", display: "flex", gap: "10px", flexDirection: "column" }}>
+      <h2>Agora Chat Examples</h2>
+      {!isLoggedIn ? (
+        <>
+          <InputField label="UserID" value={userId} onChange={setUserId} placeholder="Enter the user ID" />
+          <InputField label="Token" value={token} onChange={setToken} placeholder="Enter the token" />
+          <Button onClick={handleLogin} text="Login" />
+        </>
+      ) : (
+        <>
+          <h3>Welcome, {userId}</h3>
+          <Button onClick={handleLogout} text="Logout" />
+          <InputField label="Peer userID" value={peerId} onChange={setPeerId} placeholder="Enter the peer user ID" />
+          <InputField label="Peer message" value={message} onChange={setMessage} placeholder="Input message" />
+          <InputField label="Conversation ID" value={conversationId} onChange={setConversationId} placeholder="Enter the conversation ID" />
+          <InputField label="Conversation Type" value={conversationType} onChange={setConversationType} placeholder="Enter the conversation type" />
+          <Button onClick={handleSendMessage} text="Send" />
+        </>
+      )}
+      <Button onClick={getLocalConversations} text="Get Local Conversations" />
+      <Button onClick={fetchConversationFromServer} text="Fetch Conversations" />
+      <Button onClick={getServerPinnedMessages} text="Get Pinned Messages" />
+      <Button onClick={checkIfUserInChatroomMuteList} text="Check if the member in Chatroom Mute List" />
+      <h3>Operation log</h3>
+      <LogDisplay logs={logs} />
+    </div>
   );
 }
+
+const InputField = ({ label, value, onChange, placeholder }) => (
+  <div>
+    <label>{label}: </label>
+    <input type="text" value={value} onChange={(e) => onChange(e.target.value)} placeholder={placeholder} />
+  </div>
+);
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>{text}</button>
+);
+
+const LogDisplay = ({ logs }) => (
+  <div style={{ height: "300px", overflowY: "auto", border: "1px solid #ccc", padding: "10px", textAlign: "left" }}>
+    {logs.map((log, index) => (
+      <div key={index}>{log}</div>
+    ))}
+  </div>
+);
 
 export default App;
